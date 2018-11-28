15a573521c49b17df728f052a5c46dbb4bc97637
Lee Grignon <leegrignon@gmail.com>
2018-11-28 04:02:35 +0000

    add socket.io? support?

--- hot_tub/main.py
+++ hot_tub/main.py
@@ -7,43 +7,26 @@ from datetime import datetime, timedelta
 import time
 from lib.relay_switch import RelaySwitch
 from lib.water_temperature import WaterTemperature
+from multiprocess import Process
+
+#Create a worker process to take inputs for the hot tub
 
 class HotTub():
     def __init__(self, relay_pin=None, water_temp_pin=None):
         self.relay_pin = relay_pin
         self.water_temp_pin = water_temp_pin
 
-    def manual_switch_on_off(self, input_val=None):
-        """input val is a string, 'on' or 'off'"""
-        if input_val =='on':
-            RelaySwitch(gpio_pin=self.relay_pin).switch(1)
-        if input_val =='off':
-            RelaySwitch(gpio_pin=self.relay_pin).switch(0)
-
-    def safety_temperature_switch(self, safety_temp_f=40):
-        while WaterTemperature().measure() < safety_temp_f:
-            RelaySwitch(gpio_pin=self.relay_pin).switch(1)
-            time.sleep(5)
+    @staticmethod
+    def start_process()
+    proc = multiprocessing.Process(target=worker, args=name='tub daemon')
+    proc.daemon = True
+    proc.start()
 
     @staticmethod
-    def ceil_dt(dt, delta):
-        """Round time into x minute increments"""
-        return dt + (datetime.min - dt) % delta
+    def worker(input_dict=None):
+        relay_switch['task']()
 
-    def calendar_switch(self, run_duration=15):
-        """if the current time is in the same 15 min as the ones listed below
-           run the relay
-        """
-        #list of tuples, (run_time, duration_in_minutes)
-        run_times = [
-                {'time': '00:00:00'},
-                {'time': '00:04:00'},
-                {'time': '00:08:00'},
-                {'time': '00:12:00'},
-                {'time': '00:16:00'},
-                {'time': '00:20:00'},
-        ]
-        check_times = [self.ceil_dt(t['time'], timedelta(minutes=run_duration)) for t in run_times]
-        while self.ceil_dt(datetime.now(), timedelta(minutes=run_duration)) in check_times:
-            RelaySwitch(gpio_pin=self.relay_pin).switch(1)
-            time.sleep(5)
+if __name__ == '__main__':
+    proc = multiprocessing.Process(target=worker, name='tub daemon')
+    proc.daemon = True
+    proc.start()
