31d38010e6508375f5cf93186534d629aed97a33
Lee Grignon <leegrignon@gmail.com>
2018-12-11 19:36:48 -0800

    simplify google jobs

--- hot_tub/app/lib/controller.py
+++ hot_tub/app/lib/controller.py
@@ -1,39 +1,51 @@
+"""Server to run on a redis worker and poll internal db for changes.
+   If changes are found, execute scripts.
+"""
+from datetime import time
+import gpiozero
 from app import models
 from app.lib import rpi_job
 from app.lib.run_time import RunTime
 from app.lib.relay_switch import RelaySwitch
-from datetime import datetime
+logger = rpi_job.logger
 
-class Controller(rpi_job.RPIJob):
-    def __init__(self):
+class RelayController(rpi_job.RPIJob):
+    """Overall relay controller, runs a mock server on an infinite loop redis server.
+       Class constantly polls local db and google docs for inputs to changes
+    """
+    def __init__(self, gpio_pin=18):
         super().__init__()
+        self.relay = gpiozero.LED(gpio_pin) if self.is_rpi and gpio_pin else None
 
-    def switch_tub(self, current_state, relay):
-        if current_state.status_numeric == 1 and relay:
-            relay.on()
-        if current_state.status_numeric == 0 and relay:
-            relay.off()
+    def switch_tub(self, latest_record=None):
+        """Execute gpio pin change to relay to start/stop 3v current"""
+        cur_num = latest_record.status_numeric
+        return self.relay.on() if bool(cur_num) else self.relay.off()
 
-    def redis_server(self, gpio_pin=None):
-        relay = gpiozero.LED(18) if self.is_rpi else None
+    def redis_server(self):
+        """Server to watch for relevant databse changes"""
         while True:
-            operation_mode = self.get_operation_type()
-            safety_temp = self.get_safety_temp()
-            current_temp = models.WaterTemp.query.order_by(models.WaterTemp.id.desc()).first()
-            current_state = models.GPIOTask.query.order_by(models.GPIOTask.id.desc()).first()
-            turn_on_conditions = [safety_temp > current_temp, operation_mode == 'Turn On', bool(RunTime().check_time())] 
-            turn_off_conditions = [operation_mode == 'Turn Off', not bool(RunTime().check_time())]
-            if current_state.status_numeric == 0:
-                if bool(any(turn_on_conditions)):
+            #poll these less frequently, they require a google sheets server check
+            op_mode, safety_temp, laps = self.get_operation_type(), self.get_safety_temp(), 0
+            while laps < 60:
+                current_temp = models.WaterTemp.query.order_by(models.WaterTemp.id.desc()).first()
+                latest_record = models.GPIOTask.query.order_by(models.GPIOTask.id.desc()).first()
+                turn_on_conds = [safety_temp > current_temp, op_mode == 'Turn On', bool(RunTime().check_time())]
+                turn_off_conds = [op_mode == 'Turn Off', not bool(RunTime().check_time())]
+                cur_num = latest_record.status_numeric
+                #if the tub is off and meets any 'on' condition write to db to turn it on
+                if not bool(cur_num) and bool(any(turn_on_conds)):
                     RelaySwitch().turn_on_hot_tub()
-                continue
-            else:
-                if bool(any(turn_off_conditions)):
-                    RelaySwitch().turn_off_hot_tub()
-                continue 
 
-            self.switch_tub(current_state=current_state, relay=relay)
+                #if the tub is on and meets any 'off' condition write to db to turn it off
+                if bool(cur_num) and not (safety_temp > current_temp) and bool(any(turn_off_conds)):
+                    RelaySwitch().turn_off_hot_tub()
+
+                #check the database and execute the relay
+                self.switch_tub(latest_record=latest_record)
+                time.sleep(1)
+                laps += 1
 
 if __name__ == '__main__':
     #start a redis worker to the name 'hot_tub'
-    Controller().redis_server()
+    RelayController().redis_server()
---
+++ hot_tub/app/lib/google_jobs.py
@@ -0,0 +1,30 @@
+"""
+    Connector to pygsheets for hot tub controls.
+    Application only attempts to ping the hot_tub database every minute.
+"""
+import pygsheets
+from app.lib.config import Config
+class GoogleJobs:
+    def __init__(self):
+        self.pyg =  
+
+    def get_hot_tub_controller(self):
+        pyg = pygsheets.authorize(service_file=Config.CREDS_PATH)
+        sheet = pyg.open_by_key('1wWj0sFmgwsTRolqUV2fQqFCxxN7kWzHS-FXNtAB1ymk')
+        return sheet.worksheet_by_title('Controls')
+
+    def get_times(self):
+        """returns a matrix of times of format [[start_time, end_time], [start_time, end_time]]"""
+        ctrl = self.get_hot_tub_controller()
+        vals_matrix = ctrl.get_named_ranges('run_times').data()
+        return [val for val in vals_matrix if bool(val[0])]
+
+    def get_safety_temp(self):
+        """returns the desired safety temperature in degrees farenheit"""
+        ctrl = self.get_hot_tub_controller()
+        return ctrl.get_named_ranges('safety_temperature').data()[0][0]
+
+    def get_operation_type(self):
+        """returns tub run status (Turn On, Turn Off, Time Control)"""
+        ctrl = self.get_hot_tub_controller()
+        return ctrl.get_named_ranges('manual_operation').data()[0][0]
