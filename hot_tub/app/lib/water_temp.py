15f194177f5d09a0bccd0b107af5b2c6eb0d67c7
Lee Grignon <leegrignon@gmail.com>
2018-12-21 13:39:19 -0800

    code to record water temperature

--- hot_tub/app/lib/water_temp.py
+++ hot_tub/app/lib/water_temp.py
@@ -10,19 +10,25 @@ On the Raspberry Pi, you will need to add dtoverlay=w1-gpio" (for regular connec
 
 The default data pin is GPIO4 (RaspPi connector pin 7), but that can be changed from 4 to x with dtoverlay=w1-gpio,gpiopin=x.
 """
+import re
 from app.models import WaterTemp
+from app.lib import rpi_job
 from app import db
 from lib import rpi_job
 from w1thermsensor import W1ThermSensor
 from datetime import datetime
+logger = rpi_job.logger
 
+sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, '00000a99d508')
 
 class Temperature(rpi_job.RPIJob):
     def __init__(self):
         super().__init__()
 
     def record_temperature(self):
-        sensor = W1ThermSensor()
         temp_f = sensor.get_temperature(W1ThermSensor.DEGREES_F)
         db.session.add(WaterTemp(temperature_f=temp_f, create_datetime=datetime.now()))
         db.session.commit()
+
+if __name__ == '__main__':
+    Temperature().record_temperature()
