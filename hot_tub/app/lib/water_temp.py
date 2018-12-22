a44e761f47b89049fcc6a2c5987afcb5f9641b50
Lee Grignon <leegrignon@gmail.com>
2018-12-22 10:01:35 -0800

    setup water temp server

--- hot_tub/app/lib/water_temp.py
+++ hot_tub/app/lib/water_temp.py
@@ -1,13 +1,5 @@
 """
-   https://github.com/timofurrer/w1thermsensor
-
-The following hardware is needed:
-
-w1 therm compatible sensor (some of them can be bought here: Adafruit: DS18B20)
-wires to connect the sensor to your board (you might need a breadboard, too)
-a board like the Raspberry Pi or the Beagle Bone)
-On the Raspberry Pi, you will need to add dtoverlay=w1-gpio" (for regular connection) or dtoverlay=w1-gpio,pullup="y" (for parasitic connection) to your /boot/config.txt.
-
+https://github.com/timofurrer/w1thermsensor
 The default data pin is GPIO4 (RaspPi connector pin 7), but that can be changed from 4 to x with dtoverlay=w1-gpio,gpiopin=x.
 """
 import re
@@ -31,16 +23,17 @@ class Temperature(rpi_job.RPIJob):
         db.session.add(WaterTemp(temperature_f=temp_f, create_datetime=datetime.now()))
         db.session.commit()
 
-def temperature_server(self):
+#RQ Worker Server setup
+def temperature_server():
     """looks for a redis worker called 'hot_tub_temp' and assigns the background job forever"""
     queue = rq.Queue('hot_tub_temp', connection=Redis.from_url('redis://'))
     job = queue.enqueue('app.lib.water_temp.check_temp', -1)
 
-def check_temp(self):
+def check_temp():
     """Background job that is constantly measuring and recording water temperature
        Time delay in the file i/o is long enough to not need sleep instructions"""
     while True:
         Temperature().record_temperature()
 
 if __name__ == '__main__':
-    Temperature().record_temperature()
+    temperature_server()
