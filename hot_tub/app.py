9c90461b3ce480565d2123ece41b32f03e2e9fd1
Lee Grignon <leegrignon@gmail.com>
2018-12-24 22:07:13 -0800

    cleanup

--- hot_tub/app.py
+++ hot_tub/app.py
@@ -1,28 +1,6 @@
 from flask import Flask, jsonify, abort, make_response, request, render_template
-from lib.
 
 app = Flask(__name__)
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-@app.route('/greenhouse/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    request_task = request.json['task']
-    tasks = {
-        'random_number': RandomNumber().measure,
- #       'take_photo': Camera().take_photo,
-        'soil_sensor': SoilSensor().measure,
-        'sun_sensor': SunSensor().measure,
-        'temp_humidity_sensor': TempHumiditySensor().measure,
-        'toggle_valve': ToggleValve().toggle()
-    }
-
-    if not request.json or request_task not in tasks.keys():
-        abort(400)
-
-    result = tasks[request_task]()
-    return jsonify({'task': result}), 201
 if __name__ == '__main__':
     app.run(debug=True)
--- hot_tub/app/__init__.py
+++ hot_tub/app/__init__.py
@@ -1,12 +1,11 @@
 import sys
 import os
-from flask_login import LoginManager
+import pylint-flask
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/app/')
 
 from flask import Flask
-from flask_login import UserMixin
 
 from app.lib.admin.config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -16,6 +15,5 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-login = LoginManager(app)
 
 from app import routes, models
